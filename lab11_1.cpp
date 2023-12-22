41a0e7243c59567c6b46dbf360e7aeb408ca1f2a
rutchanon0878 <rutchanon_inkam@cmu.ac.th>
2023-12-22 15:57:22 +0700

    finish

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -12,14 +12,16 @@ int main(){
     cin.get();
 
     srand(time(0));
-    int x = rand()%8;
+    int x = rand()%9;
     if(x == 0) cout << "You will get A in this 261102.";
     else if(x == 1) cout << "You will get B+ in this 261102.";
     else if(x == 2) cout << "You will get B in this 261102.";
-    else if(x == 3) cout << "You will get D+ in this 261102.";
-    else if(x == 4) cout << "You will get D in this 261102.";
-    else if(x == 5) cout << "You will get F in this 261102.";
-    else if(x == 6) cout << "You will get W in this 261102.";
+    else if(x == 3) cout << "You will get C+ in this 261102.";
+    else if(x == 4) cout << "You will get C in this 261102.";
+    else if(x == 5) cout << "You will get D+ in this 261102.";
+    else if(x == 6) cout << "You will get D in this 261102.";
+    else if(x == 7) cout << "You will get F in this 261102.";
+    else cout << "You will get W in this 261102.";
 
     return 0;
 }
